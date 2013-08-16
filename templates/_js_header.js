139856039a7a631324dfb4b971c46f7c830de091
geraldarthur <gerald.a.rich@gmail.com>
2013-08-16 18:32:59 -0400

    update templates, less, add bootstrap 3 less

--- templates/_js_header.js
+++ templates/_js_header.js
@@ -1,7 +1,7 @@
 /*
  * Looking for the full, uncompressed source? Try here:
  *
- * https://github.com/nprapps/%s
+ * https://github.com/geraldarthur/%s
  * 
  * The following files are included in this compressed version:
  *{% for path in paths %}
